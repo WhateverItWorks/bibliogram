bfb6486c6fd3df47bc1b61d093879961061e1872
xbdm <xbdm@xbdm.fun>
2023-06-24 02:33:13 -0400

    Delete src directory

--- src/lib/utils/proxyurl.js
+++
@@ -1,8 +0,0 @@
-function proxyImage(url, width) {
-	const params = new URLSearchParams()
-	if (width) params.set("width", width)
-	params.set("url", url)
-	return "/imageproxy?"+params.toString()
-}
-
-module.exports.proxyImage = proxyImage
